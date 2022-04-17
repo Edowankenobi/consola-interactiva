ea5c0f71602217db997a435dc8af1a4bfc4d692c
Eduardo Gonzalez <edogonzalez1@gmail.com>
2022-04-16 22:02:25 -0400

    se añaden los modelos para tareas

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 require('colors');
 const { inquirerMenu,pausa } = require('./helpers/inquirer');
 
-
-
-
-console.clear();
+const Tareas = require('./models/tareas');
 
 
 const main = async() => {
@@ -14,6 +11,7 @@ const main = async() => {
     do {
         opt = await inquirerMenu();
         console.log({opt});
+
         await pausa();
         
     } while (opt !== '0');
